0965f902456a23f2e670612f85f11fbec81256af
Christian M <chriamue@gmail.com>
2024-10-11 20:33:32 +0200

    :sparkles::construction_worker: cp index html for pages

--- welcome-page/src/welcome.rs
+++ welcome-page/src/welcome.rs
@@ -5,6 +5,8 @@ pub fn welcome() -> Html {
     html! {
         <div>
             <h1>{"Welcome"}</h1>
+            <p>{"This is the welcome page."}</p>
+            <a href="p">{"Let's start"}</a>
         </div>
     }
 }
